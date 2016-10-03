c75ed50b09c6438414672e1152893e94a6da6662
Cuitarded <kristopher.cui@gmail.com>
2016-10-03 15:56:21 -0400

    Update Year20-Fast.asm

--- Year20-Fast.asm
+++ Year20-Fast.asm
@@ -9,211 +9,211 @@
     INBOX
     SUB 4
     JUMPN a
-	JUMPZ b
-	SUB 9
-	JUMPN c
-	JUMPZ d
-	SUB 9
-	JUMPN e
-	JUMPZ f
-	COPYFROM 0
-	ADD 0
-	ADD 0
-	COPYTO 0
-	JUMP g
+    JUMPZ b
+    SUB 9
+    JUMPN c
+    JUMPZ d
+    SUB 9
+    JUMPN e
+    JUMPZ f
+    COPYFROM 0
+    ADD 0
+    ADD 0
+    COPYTO 0
+    JUMP g
  a: 
- 	ADD 9
-	JUMPN i
-	JUMPZ p
-	COPYFROM 0
-	JUMP o
+    ADD 9
+    JUMPN i
+    JUMPZ p
+    COPYFROM 0
+    JUMP o
  b:
- 	ADD 0
-	ADD 0
-	COPYTO 0
-	JUMP h
+    ADD 0
+    ADD 0
+    COPYTO 0
+    JUMP h
  c:
- 	COPYFROM 0
-	ADD 0
-	COPYTO 2
-	ADD 2
-	JUMP n
+    COPYFROM 0
+    ADD 0
+    COPYTO 2
+    ADD 2
+    JUMP n
  d:
- 	COPYFROM 0
-	ADD 0
-	ADD 0
-	COPYTO 0
-	JUMP m
+    COPYFROM 0
+    ADD 0
+    ADD 0
+    COPYTO 0
+    JUMP m
  e: 
- 	COPYFROM 0
-	ADD 0
-	ADD 0
-	COPYTO 2
-	ADD 2
-	JUMP l
+    COPYFROM 0
+    ADD 0
+    ADD 0
+    COPYTO 2
+    ADD 2
+    JUMP l
  f: 
- 	ADD 0
-	ADD 0
-	ADD 0
-	ADD 0
-	COPYTO 0
-	JUMP k
+    ADD 0
+    ADD 0
+    ADD 0
+    ADD 0
+    COPYTO 0
+    JUMP k
  i:
- 	COPYFROM 0
-	JUMP j
+    COPYFROM 0
+    JUMP j
  g:
  o:
  p:
- 	ADD 0
+    ADD 0
  h:
  k:
  l:
  m:
  n:
-	ADD 0
+    ADD 0
  j:
-	OUTBOX
+    OUTBOX
     INBOX
     COPYTO 0
-	INBOX
-	SUB 4
-	JUMPN q
-	JUMPZ r
-	SUB 9
-	JUMPN s
-	JUMPZ t
-	SUB 9
-	JUMPN u
-	JUMPZ v
-	COPYFROM 0
-	ADD 0
-	ADD 0
-	COPYTO 0
-	JUMP w
+    INBOX
+    SUB 4
+    JUMPN q
+    JUMPZ r
+    SUB 9
+    JUMPN s
+    JUMPZ t
+    SUB 9
+    JUMPN u
+    JUMPZ v
+    COPYFROM 0
+    ADD 0
+    ADD 0
+    COPYTO 0
+    JUMP w
  q: 
- 	ADD 9
-	JUMPN y
-	JUMPZ af
-	COPYFROM 0
-	JUMP ae
+    ADD 9
+    JUMPN y
+    JUMPZ af
+    COPYFROM 0
+    JUMP ae
  r:
- 	ADD 0
-	ADD 0
-	COPYTO 0
-	JUMP x
+    ADD 0
+    ADD 0
+    COPYTO 0
+    JUMP x
  s:
- 	COPYFROM 0
-	ADD 0
-	COPYTO 2
-	ADD 2
-	JUMP ad
+    COPYFROM 0
+    ADD 0
+    COPYTO 2
+    ADD 2
+    JUMP ad
  t:
- 	COPYFROM 0
-	ADD 0
-	ADD 0
-	COPYTO 0
-	JUMP ac
+    COPYFROM 0
+    ADD 0
+    ADD 0
+    COPYTO 0
+    JUMP ac
  u: 
- 	COPYFROM 0
-	ADD 0
-	ADD 0
-	COPYTO 2
-	ADD 2
-	JUMP ab
+    COPYFROM 0
+    ADD 0
+    ADD 0
+    COPYTO 2
+    ADD 2
+    JUMP ab
  v: 
- 	ADD 0
-	ADD 0
-	ADD 0
-	ADD 0
-	COPYTO 0
-	JUMP aa
+    ADD 0
+    ADD 0
+    ADD 0
+    ADD 0
+    COPYTO 0
+    JUMP aa
  y:
- 	COPYFROM 0
-	JUMP z
+    COPYFROM 0
+    JUMP z
  w:
  ae:
  af:
- 	ADD 0
+    ADD 0
  x:
  aa:
  ab:
  ac:
  ad:
-	ADD 0
+    ADD 0
  z:
-	OUTBOX
-	INBOX
-	INBOX
-	OUTBOX
-	INBOX
-	OUTBOX
-	INBOX
-	INBOX
+    OUTBOX
+    INBOX
+    INBOX
+    OUTBOX
+    INBOX
+    OUTBOX
+    INBOX
+    INBOX
     COPYTO 0
-	INBOX
-	SUB 4
-	JUMPN ag
-	JUMPZ ah
-	SUB 9
-	JUMPN ai
-	JUMPZ aj
-	SUB 9
-	JUMPN ak
-	JUMPZ al
-	COPYFROM 0
-	ADD 0
-	ADD 0
-	COPYTO 0
-	JUMP am
+    INBOX
+    SUB 4
+    JUMPN ag
+    JUMPZ ah
+    SUB 9
+    JUMPN ai
+    JUMPZ aj
+    SUB 9
+    JUMPN ak
+    JUMPZ al
+    COPYFROM 0
+    ADD 0
+    ADD 0
+    COPYTO 0
+    JUMP am
  ag: 
- 	ADD 9
-	JUMPN ao
-	JUMPZ av
-	COPYFROM 0
-	JUMP au
+    ADD 9
+    JUMPN ao
+    JUMPZ av
+    COPYFROM 0
+    JUMP au
  ah:
- 	ADD 0
-	ADD 0
-	COPYTO 0
-	JUMP an
+    ADD 0
+    ADD 0
+    COPYTO 0
+    JUMP an
  ai:
- 	COPYFROM 0
-	ADD 0
-	COPYTO 2
-	ADD 2
-	JUMP at
+    COPYFROM 0
+    ADD 0
+    COPYTO 2
+    ADD 2
+    JUMP at
  aj:
- 	COPYFROM 0
-	ADD 0
-	ADD 0
-	COPYTO 0
-	JUMP as
+    COPYFROM 0
+    ADD 0
+    ADD 0
+    COPYTO 0
+    JUMP as
  ak: 
- 	COPYFROM 0
-	ADD 0
-	ADD 0
-	COPYTO 2
-	ADD 2
-	JUMP ar
+    COPYFROM 0
+    ADD 0
+    ADD 0
+    COPYTO 2
+    ADD 2
+    JUMP ar
  al: 
- 	ADD 0
-	ADD 0
-	ADD 0
-	ADD 0
-	COPYTO 0
-	JUMP aq
+    ADD 0
+    ADD 0
+    ADD 0
+    ADD 0
+    COPYTO 0
+    JUMP aq
  ao:
- 	COPYFROM 0
-	JUMP ap
+    COPYFROM 0
+    JUMP ap
  am:
  au:
  av:
- 	ADD 0
+    ADD 0
  an:
  aq:
  ar:
  as:
  at:
-	ADD 0 
+    ADD 0 
  ap:
-	OUTBOX
+    OUTBOX
