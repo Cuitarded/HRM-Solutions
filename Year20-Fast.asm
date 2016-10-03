e9c6f69fff874de5170ea1b251e75f0194062795
Cuitarded <kristopher.cui@gmail.com>
2016-10-03 15:47:22 -0400

    Update Year20-Fast.asm

--- Year20-Fast.asm
+++ Year20-Fast.asm
@@ -21,57 +21,198 @@
 	ADD 0
 	COPYTO 0
 	JUMP g
-
  a: 
  	ADD 9
 	JUMPN i
-	JUMPZ g
+	JUMPZ p
 	COPYFROM 0
-	JUMP g:
- 
+	JUMP o
  b:
  	ADD 0
 	ADD 0
 	COPYTO 0
-	JMP h
+	JUMP h
  c:
  	COPYFROM 0
 	ADD 0
 	COPYTO 2
 	ADD 2
-	JUMP h
+	JUMP n
  d:
  	COPYFROM 0
 	ADD 0
 	ADD 0
 	COPYTO 0
-	JUMP h
+	JUMP m
  e: 
  	COPYFROM 0
 	ADD 0
 	ADD 0
 	COPYTO 2
 	ADD 2
-	JUMP h
+	JUMP l
  f: 
  	ADD 0
 	ADD 0
 	ADD 0
 	ADD 0
 	COPYTO 0
-	JUMP h
+	JUMP k
  i:
  	COPYFROM 0
 	JUMP j
  g:
+ o:
+ p:
  	ADD 0
  h:
+ k:
+ l:
+ m:
+ n:
 	ADD 0
  j:
 	OUTBOX
-	
- BLAH:
-    JUMPN a
-    JUMPN a
-
-
+    INBOX
+    COPYTO 0
+	INBOX
+	SUB 4
+	JUMPN q
+	JUMPZ r
+	SUB 9
+	JUMPN s
+	JUMPZ t
+	SUB 9
+	JUMPN u
+	JUMPZ v
+	COPYFROM 0
+	ADD 0
+	ADD 0
+	COPYTO 0
+	JUMP w
+ q: 
+ 	ADD 9
+	JUMPN y
+	JUMPZ af
+	COPYFROM 0
+	JUMP ae
+ r:
+ 	ADD 0
+	ADD 0
+	COPYTO 0
+	JUMP x
+ s:
+ 	COPYFROM 0
+	ADD 0
+	COPYTO 2
+	ADD 2
+	JUMP ad
+ t:
+ 	COPYFROM 0
+	ADD 0
+	ADD 0
+	COPYTO 0
+	JUMP ac
+ u: 
+ 	COPYFROM 0
+	ADD 0
+	ADD 0
+	COPYTO 2
+	ADD 2
+	JUMP ab
+ v: 
+ 	ADD 0
+	ADD 0
+	ADD 0
+	ADD 0
+	COPYTO 0
+	JUMP aa
+ y:
+ 	COPYFROM 0
+	JUMP z
+ w:
+ ae:
+ af:
+ 	ADD 0
+ x:
+ aa:
+ ab:
+ ac:
+ ad:
+	ADD 0
+ z:
+	OUTBOX
+	INBOX
+	INBOX
+	OUTBOX
+	INBOX
+	OUTBOX
+	INBOX
+    COPYTO 0
+	INBOX
+	SUB 4
+	JUMPN ag
+	JUMPZ ah
+	SUB 9
+	JUMPN ai
+	JUMPZ aj
+	SUB 9
+	JUMPN ak
+	JUMPZ al
+	COPYFROM 0
+	ADD 0
+	ADD 0
+	COPYTO 0
+	JUMP am
+ ag: 
+ 	ADD 9
+	JUMPN ao
+	JUMPZ av
+	COPYFROM 0
+	JUMP au
+ ah:
+ 	ADD 0
+	ADD 0
+	COPYTO 0
+	JUMP an
+ ai:
+ 	COPYFROM 0
+	ADD 0
+	COPYTO 2
+	ADD 2
+	JUMP at
+ aj:
+ 	COPYFROM 0
+	ADD 0
+	ADD 0
+	COPYTO 0
+	JUMP as
+ ak: 
+ 	COPYFROM 0
+	ADD 0
+	ADD 0
+	COPYTO 2
+	ADD 2
+	JUMP ar
+ al: 
+ 	ADD 0
+	ADD 0
+	ADD 0
+	ADD 0
+	COPYTO 0
+	JUMP aq
+ ao:
+ 	COPYFROM 0
+	JUMP ap
+ am:
+ au:
+ av:
+ 	ADD 0
+ an:
+ aq:
+ ar:
+ as:
+ at:
+	ADD 0
+ ap:
+	OUTBOX
